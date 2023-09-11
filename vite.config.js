00194b57d240902994c121526a3e39db86f04748
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-09-11 07:43:41 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/app.scss'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/app.scss', 'public/css/auth.css'],
             refresh: true,
         }),
     ],
