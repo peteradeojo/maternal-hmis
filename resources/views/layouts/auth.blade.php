b6bb599b0fee3948e560584ffdbcf3dd211e03d3
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-09-11 08:19:41 +0100

    content security policy

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css"
         integrity="sha512-rt/SrQ4UNIaGfDyEXZtNcyWvQeOq0QLygHluFQcSjaGB04IxWhal71tKuzP6K8eYXYB6vJV4pHkXcmFGGQ1/0w=="
