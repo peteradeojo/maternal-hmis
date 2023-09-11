e5ad3a1cb971aad10e79b0af5c298439115e7c59
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-09-11 07:49:18 +0100

    vite config & Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/app.scss', 'public/css/auth.css'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/util.js",
+                "resources/css/app.scss",
+                "public/css/auth.css",
+            ],
             refresh: true,
         }),
     ],
