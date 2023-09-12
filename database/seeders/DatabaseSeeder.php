691597fe3a64ef17768f2b4c509cffffdc920695
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-09-12 09:18:09 +0100

    updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Enums\Department;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::updateOrCreate(['phone' => '08103490675'], [
+            'firstname' => 'Boluwatife',
+            'lastname' => 'Ade-Ojo',
+            'phone' => '08103490675',
+            'password' => Hash::make(env('DEFAULT_PASSWORD', 'password')),
+            'department_id' => Department::IT->value,
+        ]);
         (new CategorySeeder)->run();
         (new DepartmentSeeder)->run();
     }
