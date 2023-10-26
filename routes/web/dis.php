4e729fc1ed5d409c61d950e00f4b477a0224a6c1
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-10-26 18:22:05 +0100

    fix: prescription list

--- routes/web/dis.php
+++ routes/web/dis.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\PharmacyController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('dis.')->prefix('dispensary')->group(function (){
-    Route::get('/prescriptions', [PharmacyController::class, 'index'])->name('index');
+    Route::get('/prescriptions', [PharmacyController::class, 'dispensaryIndex'])->name('index');
     Route::match(['get', 'post'], '/prescriptions/{doc}', [PharmacyController::class, 'dispensaryShow'])->name('get-prescription');
 });
