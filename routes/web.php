fe1d47613863c9c48b97b04f656a6217b0521680
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-10-27 13:11:38 +0100

    Routec= fix

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
-
 Route::get('/login', function () {
     return view('login');
 });
