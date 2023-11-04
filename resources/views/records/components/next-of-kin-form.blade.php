e4bd3ac92565a22038a2c21aebd8772d5c0683ea
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-11-04 18:04:59 +0100

    fix

--- resources/views/records/components/next-of-kin-form.blade.php
+++ resources/views/records/components/next-of-kin-form.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group">
     <label for="nok_name">Next of Kin Name</label>
-    <input type="text" name="nok_name" id="nok_name" class="form-control" value="{{ $patient?->nok_name }}" />
+    <input type="text" name="nok_name" id="nok_name" class="form-control" value="" />
 </div>
 <div class="form-group">
     <label for="nok_phone">Next of Kin Phone</label>
-    <input type="text" name="nok_phone" id="nok_phone" class="form-control" value="{{ $patient?->nok_phone }}" />
+    <input type="text" name="nok_phone" id="nok_phone" class="form-control" value="" />
 </div>
 <div class="form-group">
     <label for="nok_address">Next of Kin Address</label>
-    <textarea name="nok_address" id="nok_address" class="form-control">{{ $patient?->nok_address }}</textarea>
+    <textarea name="nok_address" id="nok_address" class="form-control"></textarea>
 </div>
