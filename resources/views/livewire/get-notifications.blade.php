1420692ee9b38c93c2d90d72b3bc79ecf8a7aae6
Boluwatife <adeojopeter@gmail.com>
2024-09-23 03:27:33 +0100

    updates

--- resources/views/livewire/get-notifications.blade.php
+++ resources/views/livewire/get-notifications.blade.php
@@ -1,12 +1,13 @@
 <div wire:poll.5s>
-    <p class="mb-1"><a href="#" wire:click='clearAll'>Clear</a></p>
+    <p class="mb-1"><a class="text-blue-700 underline" href="#" wire:click='clearAll'>Clear</a></p>
 
     @forelse ($notifications as $n)
-        <div class="card py mb-1 px bg-red">
-            <div class="body">
+        <div class="p-2 mb-2 rounded bg-red-500">
+            <div class="body text-sm">
                 {!! $n->data['message'] !!}<br>
                 <small>{{ $n->created_at->diffForHumans() }}</small>
-                <small wire:click="clear('{{ $n->id }}')"><u class="text-white" style="cursor: pointer">Clear</u></small>
+                <small wire:click="clear('{{ $n->id }}')"><u class="text-white"
+                        style="cursor: pointer">&times;</u></small>
             </div>
         </div>
     @empty
