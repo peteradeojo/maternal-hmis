cb591335ac49ede62da2d921020130d2433e61b8
Boluwatife <adeojopeter@gmail.com>
2024-09-26 11:14:49 +0100

    debounce dynamic search

--- resources/views/livewire/dynamic-product-search.blade.php
+++ resources/views/livewire/dynamic-product-search.blade.php
@@ -1,8 +1,8 @@
 <div>
     {{-- The whole world belongs to you. --}}
     <div class="relative border">
-        <input type="search" wire:model.live="queryString" wire:keyup.debounce.250ms="searchProducts"
-            class="w-full relative" wire:blur.debounce.1s="resetResults" wire:focus.debounce.250ms="searchProducts"
+        <input type="search" wire:model.live.debounce.2s="queryString" wire:keyup.debounce.250ms="searchProducts"
+            class="w-full relative" wire:blur.debounce.2s="resetResults" wire:focus.debounce.250ms="searchProducts"
             placeholder="Enter Item" />
         @if ($display && count($results ?? []) > 0)
             <ul class="relative bottom-0 border border-black sp-list max-h-80 overflow-y-auto">
