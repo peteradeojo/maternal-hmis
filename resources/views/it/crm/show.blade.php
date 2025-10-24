2b5779518984e660199caf0b2609fd8d27c76658
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-10-24 18:16:33 +0100

    fi show crm view

--- resources/views/it/crm/show.blade.php
+++ resources/views/it/crm/show.blade.php
@@ -5,7 +5,7 @@
         <div class="card p-1">
             <h1 class="text-3xl font-bold">{{ $post->title }}</h1>
             <p><small>Posted: <i>{{ $post->created_at?->format('Y-m-d H:i A') }}</i> by
-                    <u>{{ $post->user->name }}</u></small></p>
+                    <u>{{ $post->user }}</u></small></p>
             <div>{!! $data !!}</div>
         </div>
     </div>
