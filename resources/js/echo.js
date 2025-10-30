fa799e04041e2f636c823538d503fdfc776a618e
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-10-30 13:39:33 +0100

    echo hostname

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -8,7 +8,7 @@ const hostname = window.location.hostname;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: hostname, // import.meta.env.VITE_REVERB_HOST,
+    wsHost: hostname == "portal.maternalchildhosp.com" ? "reverb.maternalchildhosp.com" : hostname, // import.meta.env.VITE_REVERB_HOST,
     wsPort: 8080, // import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: 443, // import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: ['localhost', '127.0.0.1', '100.114.76.111', 'portal', 'portal.lan', '192.168.0.171'].includes(hostname) == false,
