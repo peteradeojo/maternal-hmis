abe9e000e9f8779f3b6dc1f7376ef107363d1863
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-10-31 17:15:22 +0100

    feat: health check

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/health-check', function () {
+    return response()->json(['status' => 'ok'], 200);
+})->middleware('throttle:10,1');
+
 Route::get('/login', function () {
     return view('login');
 });
