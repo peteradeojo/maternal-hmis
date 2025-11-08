89df25a4adba32a0287bdb05d47a576865dc3b99
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-11-08 19:56:13 +0100

    fix: soft delete prescriptions

--- app/Models/DocumentationPrescription.php
+++ app/Models/DocumentationPrescription.php
@@ -6,10 +6,11 @@ use App\Enums\EventLookup;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DocumentationPrescription extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = null;
 
