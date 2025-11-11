9db411955c84b8dc13a019fe5ae81b2703092306
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-11-11 09:53:26 +0100

    omo

--- app/Http/Controllers/LabController.php
+++ app/Http/Controllers/LabController.php
@@ -261,7 +261,7 @@ class LabController extends Controller
     {
         $query = Visit::with(['patient.category', 'visit'])->has('tests')->orWhereHas('visit', function ($query) {
             $query->has('tests');
-        })->latest()->orderBy('status');
+        })->latest(); //->orderBy('status');
         return $this->dataTable($request, $query, [
             function ($query, $search) {
                 $query->whereHas('patient', function ($query) use ($search) {
