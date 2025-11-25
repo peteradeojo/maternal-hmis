990d88c98cfcd01cad1d73fc120df94569154e53
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-11-25 17:07:29 +0100

    fix: editable note

--- resources/views/components/editable-note.blade.php
+++ resources/views/components/editable-note.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ editing: false, note: @js($note) }" @keyup.escape="note.note='{{ $note->note }}';editing=false">
+<div x-data="{ editing: false, note: @js($note) }" @keyup.escape="note.note=`{{ $note->note }}`;editing=false">
     <div x-show="!editing" class="bg-gray-100 p-2">
         <p x-text="note.note"></p>
         <p><b>Attending:</b> <span x-text="note.consultant?.name"></span></p>
@@ -12,17 +12,17 @@
     <div x-show="editing" class="bg-gray-100 p-2">
         <x-input-textarea x-model="note.note" name="note" class="mb-2 form-control"
             @keydown.enter.ctrl="updateNote(note.id, note.note).catch((err) => {
-                note.note = '{{ $note->note }}';
+                note.note = `{{ $note->note }}`;
             }).finally(() => editing = false)" />
 
         <button class="btn btn-sm text-white bg-green-500"
             @click="updateNote(note.id, note.note).catch((err) => {
-                note.note = '{{ $note->note }}';
+                note.note = `{{ $note->note }}`;
             }).finally(() => editing = false)">Update
             <i class="fa fa-save"></i></button>
 
         <button class="btn btn-sm text-white bg-red-500"
-            @click="note.note = '{{ $note->note }}';editing = false;">Cancel
+            @click="note.note = `{{ $note->note }}`;editing = false;">Cancel
             <i class="fa fa-stop"></i></button>
     </div>
 </div>
