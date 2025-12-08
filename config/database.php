2681af493ffa60ea1e3220128fa7bbc8d1c25908
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-12-08 17:05:36 +0100

    fix: db schema

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => 'public,hmis',
             'sslmode' => 'prefer',
             'schema' => env('DB_SCHEMA', 'public'),
         ],
