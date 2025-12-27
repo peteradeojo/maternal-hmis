cebbe4408b6206af779b2349c1f29973864f7db0
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-12-27 19:20:46 +0100

    fix: composite key migration unfeasible in production

--- database/migrations/2025_12_23_135739_add_composite_unique_index_to_patient_imagings.php
+++ database/migrations/2025_12_23_135739_add_composite_unique_index_to_patient_imagings.php
@@ -4,26 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('patient_imagings', function (Blueprint $table) {
-            $table->unique(['documentable_type', 'documentable_id', 'describable_type', 'describable_id']);
+            // $table->unique(['documentable_type', 'documentable_id', 'describable_type', 'describable_id']);
         });
     }
 
     /**
      * Reverse the migrations.
-    */
+     */
     public function down(): void
     {
         Schema::table('patient_imagings', function (Blueprint $table) {
             //
-            $table->dropUnique(['documentable_type', 'documentable_id', 'describable_type', 'describable_id']);
+            // $table->dropUnique(['documentable_type', 'documentable_id', 'describable_type', 'describable_id']);
         });
     }
 };
