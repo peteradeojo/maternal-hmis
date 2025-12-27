ad23a87f7eef7e91d4e685cd44432aff0574366a
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-12-27 06:50:00 +0100

    adding commands for quick stats view

---
+++ app/Console/Commands/ViewStats.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ViewStats extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:view-stats {--option=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    protected $stats = [
+        'nginx_uptime' => "awk -F'backend:' '{print $2}' /var/log/nginx/hmis_backend.log | awk '{print $1}' | sort | uniq -c",
+    ];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if ($this->option('option')) {
+            $option = $this->option('option');
+            return $this->__call($option, []);
+        } else {
+            $this->info("Usage: php artisan app:view-stats --option");
+            $this->info("Usage: php artisan app:view-stats --list-options (view available options)");
+        }
+    }
+
+    public function nginx_uptime() {}
+
+    public function __call($method, $parameters)
+    {
+        if (in_array($method, array_keys($this->stats))) {
+            $cmd = $this->stats[$method];
+            $output = [];
+            $result = 100;
+            exec($cmd, $output, $result);
+            $this->info(implode("\n", $output));
+            return;
+        }
+
+        $this->error("Error: requested stats do not exist.");
+        exit(1);
+    }
+}
