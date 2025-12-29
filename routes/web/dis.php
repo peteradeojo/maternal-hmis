1eb31369b5852879b8dfff1bc7280a34f99c6597
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-12-29 09:00:38 +0100

    pharmacy access to dispensary

--- routes/web/dis.php
+++ routes/web/dis.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PharmacyController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('dis.')->prefix('dispensary')->middleware(['role:billing', 'datalog'])->group(function () {
+Route::name('dis.')->prefix('dispensary')->middleware(['role:billing|pharmacy', 'datalog'])->group(function () {
     Route::get('/prescriptions', [PharmacyController::class, 'dispensaryIndex'])->name('index');
     Route::match(['get', 'post'], '/entry', [PharmacyController::class, 'dispensaryShow'])->name('get-prescription');
 
