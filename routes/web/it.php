29bc1262eae95698b5aa495a9f7c6ee846c68530
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2025-12-30 07:23:21 +0100

    fix middleware

--- routes/web/it.php
+++ routes/web/it.php
@@ -9,31 +9,33 @@ use App\Http\Controllers\IT\ProductsController;
 use App\Http\Middleware\VerifyCsrfToken;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
-Route::name('it.')->middleware(['role:admin'])->group(function () {
-    Route::get('/department/{dep}', [StaffController::class, 'department'])->name('department');
+Route::name('it.')->group(function () {
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/department/{dep}', [StaffController::class, 'department'])->name('department');
 
-    Route::match(['get', 'post'], '/wards', [AdmissionsController::class, 'wards'])->name('wards');
+        Route::match(['get', 'post'], '/wards', [AdmissionsController::class, 'wards'])->name('wards');
 
-    Route::match(['get', 'post'], '/staff', [StaffController::class, 'index'])->name('staff');
-    Route::match(['get', 'post'], '/staff/{user}', [StaffController::class, 'show'])->name('staff.view');
-    Route::get('/info', function () {
-        return response(phpinfo(), 200)
-            ->header('Content-Type', 'text/html');
+        Route::match(['get', 'post'], '/staff', [StaffController::class, 'index'])->name('staff');
+        Route::match(['get', 'post'], '/staff/{user}', [StaffController::class, 'show'])->name('staff.view');
+        Route::get('/info', function () {
+            return response(phpinfo(), 200)
+                ->header('Content-Type', 'text/html');
+        });
+
+        Route::prefix('products')->group(function () {
+            Route::get('/', [ProductsController::class, 'index'])->name('products');
+            Route::post('/', [ProductsController::class, 'addProducts']);
+            Route::get('/get-products', [ProductsController::class, 'fetchProducts'])->middleware(['api', 'auth'])->name('get-products');
+            Route::match(['GET', 'POST'], '/get-products/{product}', [ProductsController::class, 'show'])->name('show-product');
+        });
+        Route::view('/logs', 'it.logs');
     });
 
-    Route::prefix('products')->group(function () {
-        Route::get('/', [ProductsController::class, 'index'])->name('products');
-        Route::post('/', [ProductsController::class, 'addProducts']);
-        Route::get('/get-products', [ProductsController::class, 'fetchProducts'])->middleware(['api', 'auth'])->name('get-products');
-        Route::match(['GET', 'POST'], '/get-products/{product}', [ProductsController::class, 'show'])->name('show-product');
-    });
-
-    Route::prefix('/crm')->middleware(['role:media'])->group(function () {
+    Route::prefix('/crm')->middleware(['role:media|admin'])->group(function () {
         Route::get('/', [CrmController::class, 'index'])->name('crm-index');
         Route::get('/publish', [CrmController::class, 'create'])->name('crm-publish');
         Route::post('/publish', [CrmController::class, 'publish']);
         Route::get('/{post}', [CrmController::class, 'show'])->name('crm-show');
     });
 
-    Route::view('/logs', 'it.logs');
 });
