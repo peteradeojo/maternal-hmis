32fb3929a03901ac904d1270727849584e168eea
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2026-01-03 20:36:50 +0100

    added two stat commands

--- app/Console/Commands/ViewStats.php
+++ app/Console/Commands/ViewStats.php
@@ -22,6 +22,8 @@ class ViewStats extends Command
 
     protected $stats = [
         'nginx_uptime' => "awk -F'backend:' '{print $2}' /var/log/nginx/hmis_backend.log | awk -F' status:' '{print $1}' | sed -e 's/ : /\\n/g' | sort | uniq -c",
+        'uptime' => 'w',
+        'storage' => null,
     ];
 
     /**
@@ -53,6 +55,12 @@ class ViewStats extends Command
     {
         if (in_array($method, array_keys($this->stats))) {
             $cmd = $this->stats[$method];
+
+            if (empty($cmd)) {
+                if (!method_exists($this, $method)) $this->fail("Method not exists.");
+                return $this->{$method}(...$parameters);
+            }
+
             $output = [];
             $output = shell_exec($cmd);
             $this->info($output);
@@ -61,5 +69,20 @@ class ViewStats extends Command
 
         $this->error("Error: requested stats do not exist.");
         exit(1);
+    }
+
+    protected function storage($html = false) {
+        $output = shell_exec("df -h -t ext4");
+        $output = array_filter(explode("\n", $output));
+        array_shift($output);
+
+        function parseDiskStorage($line, $that) {
+            return [$name, $size, $used, $available, $use_percent, $mount] = array_values(array_filter(explode(" ", $line)));
+        }
+
+        foreach($output as $line) {
+            [$name, $size, $used, $available, $use_percent, $mount] = parseDiskStorage($line, $this);
+            echo "Disk: $name\nUsed: $used/$size ($use_percent)\nAvailable: $available/$size\nMounted on: $mount\n\n";
+        }
     }
 }
