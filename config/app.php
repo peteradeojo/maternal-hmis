1dd26f12ca9af5e06704afa3fac2095de9f3e21e
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2026-01-09 19:10:58 +0100

    fix: libsql driver

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         HelperProvider::class,
+        \Turso\Driver\Laravel\LibSQLDriverServiceProvider::class,
     ])->toArray(),
 
     /*
