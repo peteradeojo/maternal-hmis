a8c8237e55a81fadbce30d66f7263fa6501932f6
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2026-01-10 23:04:45 +0100

    freeze nav drop downs

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -10,10 +10,17 @@
         </li>
 
         @foreach ($routes as $map)
-            <li class="p-1 nav-item border-b-2" x-data="{ open: false, toggle() {} }">
+            <li class="p-1 nav-item border-b-2" x-data='{
+                open: false,
+                toggle: function() {
+                    this.open = !this.open;
+                    localStorage.setItem(`nav-${this.label}`, this.open ? 1 : 0);
+                },
+                label: @json($map['label'][0])
+            }' x-init="open = localStorage.getItem(`nav-${label}`) == 1">
                 <div class="flex-center gap-x-2 cursor-pointer text-white hover:text-blue-300 duration-200"
-                    x-on:click="open = !open">
-                    <i class="fa {{@$map['label'][1]}}"></i>
+                    x-on:click="toggle()">
+                    <i class="fa {{ @$map['label'][1] }}"></i>
                     <span>{{ @ucfirst($map['label'][0]) }}</span>
                     <i class="fa ml-auto" :class="{ 'fa-caret-right': !open, 'fa-caret-down': open, }"></i>
                 </div>
