ceb9cddccb26a67571c8d28e10124ef771edac7f
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2026-09-02 16:32:18 +0100

    extend columns

---
+++ database/migrations/2026_09_02_162850_adjust_job_openings_columns.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('job_openings', function (Blueprint $table) {
+            $table->string('description', 512)->change();
+            $table->string('responsibilities', 512)->change();
+            $table->string('requirements', 512)->change();
+            $table->string('summary', 512)->change();
+            $table->string('footer', 512)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::table('job_openings', function (Blueprint $table) {
+            $table->string('description', 512);
+            $table->string('responsibilities', 512);
+            $table->string('requirements', 512);
+            $table->string('summary', 512);
+            $table->string('footer', 512);
+        });
+    }
+};
