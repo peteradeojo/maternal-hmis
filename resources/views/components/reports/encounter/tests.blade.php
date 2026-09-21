1874260cf437fae9d8aac871ae3ae62f8773bd56
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2026-09-21 12:08:07 +0100

    fix: admission tests

--- resources/views/components/reports/encounter/tests.blade.php
+++ resources/views/components/reports/encounter/tests.blade.php
@@ -1,8 +1,13 @@
 <div class="py-2">
     <p class="text-lg font-semibold">Test Results</p>
+    {{-- @dump($visit::class) --}}
 
     @include('doctors.components.test-results', [
-        'tests' => $visit->tests->merge($visit->visit?->tests ?? []),
+        'tests' => match ($visit::class) {
+            'App\Models\AdmissionPlan' => $visit->tests,
+            'App\Models\Admission' => $visit->tests->merge($visit->plan?->tests),
+            default => $visit->tests->merge($visit->visit?->tests),
+        },
         'cancellable' => false,
     ])
 </div>
